2f60ecf6be7709640597c99ef0125e949dfc7884
Fernando R <rosalesvegafernando@gmail.com>
2025-06-20 18:12:45 -0600

    feat: add packages and setup the db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CONFIG_DATABASE } from './common/infraestructure/config-database';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CONFIG_DATABASE(),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
