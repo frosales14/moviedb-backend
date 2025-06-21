f5323044debd360a7492fe99ad5f4053b863e478
Fernando R <rosalesvegafernando@gmail.com>
2025-06-20 22:36:39 -0600

    feat: setup auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { CONFIG_DATABASE } from './common/infraestructure/config-database';
 import { AuthModule } from './auth/auth.module';
@@ -11,7 +9,7 @@ import { AuthModule } from './auth/auth.module';
     CONFIG_DATABASE(),
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
